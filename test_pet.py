ea3cffd2f97bbc72bbfe7979704029f6fa8643de
Teelybird <scteel94@gmail.com>
2025-08-20 21:31:08 -0400

    Test results for Scott

--- test_pet.py
+++ test_pet.py
@@ -2,6 +2,7 @@ from jsonschema import validate
 import pytest
 import schemas
 import api_helpers
+import json 
 from hamcrest import assert_that, contains_string, is_
 
 '''
@@ -26,7 +27,7 @@ TODO: Finish this test by...
 3) Validate the 'status' property in the response is equal to the expected status
 4) Validate the schema for each object in the response
 '''
-@pytest.mark.parametrize("status", [("available")])
+@pytest.mark.parametrize("status", [("available","sold","pending")])
 def test_find_by_status_200(status):
     test_endpoint = "/pets/findByStatus"
     params = {
@@ -35,12 +36,25 @@ def test_find_by_status_200(status):
 
     response = api_helpers.get_api_data(test_endpoint, params)
     # TODO...
+    assert response.status_code == 200
+    resp_data = response.json()
+    assert resp_data[0]['status'] == status[0]
+    for data_obj in resp_data:
+        validate(instance=data_obj, schema=schemas.pet)
 
 '''
 TODO: Finish this test by...
 1) Testing and validating the appropriate 404 response for /pets/{pet_id}
 2) Parameterizing the test for any edge cases
 '''
-def test_get_by_id_404():
+@pytest.mark.parametrize("id", [(9999,-1)])
+
+def test_get_by_id_404(id):
     # TODO...
-    pass
+
+    for id in id:
+        test_endpoint = "/pets/" + str(id)
+        response = api_helpers.get_api_data(test_endpoint)
+        print("Tested parameter: " + str(id))
+        print("Response Code: " + str(response.status_code))
+        assert response.status_code == 404
--- test_store.py
+++ test_store.py
@@ -12,5 +12,20 @@ TODO: Finish this test by...
 3) Validate the response codes and values
 4) Validate the response message "Order and pet status updated successfully"
 '''
-def test_patch_order_by_id():
-    pass
+@pytest.mark.parametrize("status", [("available","sold","pending")])
+def test_patch_order_by_id(status):
+
+    for status in status:
+        test_endpoint = "/store/order/1"
+        patch_data = {
+            "pet_id": 1,
+            'status': status}  
+
+        response = api_helpers.patch_api_data(test_endpoint, patch_data)
+        print (response.json()) 
+        assert response.status_code == 200      
+        assert_that(response.json()['message'], contains_string("Order and pet status updated successfully"))
+        assert_that(response.json()['pet_id'], is_(1))
+        assert_that(response.json()['status'], is_(status))
+
+# Order endopoint not implemented so this test will fail
